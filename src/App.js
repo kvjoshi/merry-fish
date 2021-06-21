647af8f4bdc4dbd4f2b0b5f6db015059cbd5e6a3
Karmavir Joshi <karmavirj@live.com>
2021-06-22 03:12:26 +0530

    table added and complete firebase auth

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
-import "./App.css";
-import React from "react";
-import Signup from "./components/signup/signup";
-import {Container,Row, Col,Alert} from "react-bootstrap";
-
-import { Windmill } from '@windmill/react-ui'
-
-import { AuthProvider } from "./components/contexts/AuthContext";
+import React from "react"
+import Signup from "./components/signup/signup"
+import { Container } from "react-bootstrap"
+import { AuthProvider } from "./contexts/AuthContext"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Dashboard from "./components/dashboard/dashboard"
+import Login from "./components/login/Login"
+import PrivateRoute from "./routes/PrivateRoute"
+import ForgotPassword from "./components/forgotpassword/ForgotPassword"
+import UpdateProfile from "./components/updateprofile/UpdateProfile"
 
 function App() {
-  return (
-    <AuthProvider>
-        <Windmill>
-      <Container
-        className="d-flex align-items-center justyfy-content-center"
-        style={{ minHeight: "100vh" }}
-      >
-        <div className="w-100" style={{ maxWidth: "400px" }}>
-          <Signup />
-        </div>
-      </Container>
-        </Windmill>
-    </AuthProvider>
-  );
+    return (
+        <Container
+            className="d-flex align-items-center justify-content-center"
+            style={{ minHeight: "100vh" }}
+        >
+            <div className="w-100" style={{ maxWidth: "400px" }}>
+                <Router>
+                    <AuthProvider>
+                        <Switch>
+                            <PrivateRoute exact path="/" component={Dashboard} />
+                            <PrivateRoute path="/update-profile" component={UpdateProfile} />
+                            <Route path="/signup" component={Signup} />
+                            <Route path="/login" component={Login} />
+                            <Route path="/forgot-password" component={ForgotPassword} />
+                        </Switch>
+                    </AuthProvider>
+                </Router>
+            </div>
+        </Container>
+    )
 }
 
-export default App;
+export default App
--- src/components/signup/signup.js
+++ src/components/signup/signup.js
@@ -1,64 +1,64 @@
-import { Alert } from "bootstrap";
-import React, { useRef } from "react";
-import { useState } from "react";
-import { Form, Card, Button, FormGroup } from "react-bootstrap";
-import { useAuth } from "../contexts/AuthContext";
+import React, { useRef, useState } from "react"
+import { Form, Button, Card, Alert } from "react-bootstrap"
+import { useAuth } from "../../contexts/AuthContext"
+import { Link, useHistory } from "react-router-dom"
 
 export default function Signup() {
-  const emailRef = useRef();
-  const passwordRef = useRef();
-  const passwordConfirmRef = useRef();
-  const { signup } = useAuth();
-  const [err, setErr] = useState("");
+  const emailRef = useRef()
+  const passwordRef = useRef()
+  const passwordConfirmRef = useRef()
+  const { signup } = useAuth()
+  const [error, setError] = useState("")
   const [loading, setLoading] = useState(false)
+  const history = useHistory()
 
-
-  function handleSubmit(e) {
-    e.preventDefault();
+  async function handleSubmit(e) {
+    e.preventDefault()
 
     if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-      return setErr("Try Again password do not match");
+      return setError("Passwords do not match")
     }
 
     try {
-      setErr("")
+      setError("")
       setLoading(true)
-      signup(emailRef.current.value, passwordRef.current.value);
+      await signup(emailRef.current.value, passwordRef.current.value)
+      history.push("/")
     } catch {
-      setErr("Failed to create an account!");
+      setError("Failed to create an account")
     }
+
     setLoading(false)
   }
 
   return (
-    <>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Sign Up</h2>
-          {err &&   <Alert ></Alert>}
-          <Form onSubmit = {handleSubmit}>
-            <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" ref={emailRef} required />
-            </Form.Group>
-            <Form.Group id="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" ref={passwordRef} required />
-            </Form.Group>
-            <Form.Group id="password-confirm">
-              <Form.Label>Password confirm</Form.Label>
-              <Form.Control type="password" ref={passwordConfirmRef} required />
-            </Form.Group>
-            <Button disabled={loading} type="submit" className="w-100 btn btn-default">
-            
-              Sign Up
-            </Button>
-          </Form>
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2">
-        Already have an account? Log In
-      </div>
-    </>
-  );
+      <>
+        <Card>
+          <Card.Body>
+            <h2 className="text-center mb-4">Sign Up</h2>
+            {error && <Alert variant="danger">{error}</Alert>}
+            <Form onSubmit={handleSubmit}>
+              <Form.Group id="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" ref={emailRef} required />
+              </Form.Group>
+              <Form.Group id="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" ref={passwordRef} required />
+              </Form.Group>
+              <Form.Group id="password-confirm">
+                <Form.Label>Password Confirmation</Form.Label>
+                <Form.Control type="password" ref={passwordConfirmRef} required />
+              </Form.Group>
+              <Button disabled={loading} className="w-100" type="submit">
+                Sign Up
+              </Button>
+            </Form>
+          </Card.Body>
+        </Card>
+        <div className="w-100 text-center mt-2">
+          Already have an account? <Link to="/login">Log In</Link>
+        </div>
+      </>
+  )
 }
